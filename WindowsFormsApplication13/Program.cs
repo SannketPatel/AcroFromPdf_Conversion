cec77df10673a058c893af1301ae894ed9c27e90
LAPTOP-2JLDSQ7K\Sanket-HP <sanket972528@gmail.com>
2019-10-01 16:56:27 +0530

    first commit

--- WindowsFormsApplication13/Program.cs
+++ WindowsFormsApplication13/Program.cs
@@ -13,6 +13,8 @@ namespace WindowsFormsApplication13
         [STAThread]
         static void Main()
         {
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
